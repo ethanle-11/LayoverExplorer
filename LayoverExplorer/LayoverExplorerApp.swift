dc4d0f962c2649df3f5909363a5da0e6b0117b18
Ethan Le <ethanle@unc.edu>
2026-04-11 18:37:09 -0400

    Initial Commit

---
+++ LayoverExplorer/LayoverExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoverExplorerApp.swift
+//  LayoverExplorer
+//
+//  Created by Ethan Le on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LayoverExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
